940510687edfa7e782b8c3b4cde2be178689152a
Abhinav Thakur <abhiinavthakur2003@gmail.com>
2026-05-16 03:54:44 +0530

    Update salarypredictor_ui.py

--- salarypredictor_ui.py
+++ salarypredictor_ui.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model=joblib.load(r'C:\Users\thaku\Desktop\My YouTube\Machine Learning\salary_prediction_model.joblib')
+model = joblib.load('salary_prediction_model.joblib')
 st.set_page_config(page_title = "Salary Predictor", page_icon = "💸", layout = "centered")
 
 st.title('Salary Prediction 💰💸')
